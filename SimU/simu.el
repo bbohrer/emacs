318af7443e96bfd5188fc4033d465285ab70472c
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2013-08-21 11:17:01 -0400

    Added finance screen.

--- SimU/simu.el
+++ SimU/simu.el
@@ -3,8 +3,8 @@
 (eval-when-compile
   (require 'wid-edit))
 
-(defvar simu-uni-name)
-(defvar simu-player-name)
+(defvar uni-name)
+(defvar player-name)
 
 
 (defun simu ()
@@ -15,9 +15,9 @@
   (start-screen)
 )
 
-(defvar simu-start-player-name)
-(defvar simu-start-univ-name)
-(defvar simu-start-start-button)
+(defvar start-player-name)
+(defvar start-univ-name)
+(defvar start-start-button)
 
 (defun force-erase-buffer ()
   (let ((inhibit-read-only t))
@@ -40,73 +40,143 @@ and elite little private school, an elite not-so-little
 private school, or burgeoning state school. 
 
 ")
-  (setq simu-start-player-name (widget-create 'editable-field
+  (setq start-player-name (widget-create 'editable-field
            :size 13
            :format "What is your name?: %v " ; Text after the field!
            "Spartacus"))
   (widget-insert "\n\n")
-  (setq simu-start-univ-name (widget-create 'editable-field
+  (setq start-univ-name (widget-create 'editable-field
            :size 13
            :format "What is your school named?: %v " ; Text after the field!
            "Roman Institute of Technology"))
   (widget-insert "\n\n")
-  (setq simu-start-start-button (widget-create 'push-button
+  (setq start-start-button (widget-create 'push-button
                  :notify (lambda (&rest ignore)
-                           (setq simu-univ-name (widget-value simu-start-univ-name))
-                           (setq simu-player-name (widget-value simu-start-player-name))
-                           (widget-delete simu-start-univ-name)
-                           (widget-delete simu-start-player-name)
-                           (widget-delete simu-start-start-button)
+                           (setq univ-name (widget-value start-univ-name))
+                           (setq player-name (widget-value start-player-name))
+                           (widget-delete start-univ-name)
+                           (widget-delete start-player-name)
+                           (widget-delete start-start-button)
                            (main-screen))
                  "Let's Go!"))
     (widget-setup))
 
 (defun spacer () (widget-insert "\n\n"))
 
- (defun main-screen ()
-   "Primary game screen - allows selecting between other screens"
-   (force-erase-buffer)
-   (widget-insert (format "You are %s, supreme ruler of %s.
-From here you can see all your dominion:\n\n" simu-player-name simu-univ-name))
-   (setq simu-main-finance (widget-create 'push-button
-                                          :notify (lambda (&rest ignore)
-                                                    (cleanup-main)
-                                                    (finance-screen))
-                                          "Bursar's Office"))
-   (spacer) 
-   (setq simu-main-admissions (widget-create 'push-button
-                                             :notify (lambda (&rest ignore)
-                                                       (cleanup-main)
-                                                       (admissions-screen))
-                                             "Admissions Office"))
-   (spacer)
-   (setq simu-main-academics (widget-create 'push-button
+(defun cleanup-main ()
+  "Clear all the widgets created by the main play screen"
+  (widget-delete main-finance)
+  (widget-delete main-admissions)
+  (widget-delete main-academics)
+  (widget-delete main-housing)
+  (widget-delete main-dining)
+  (widget-delete main-sports)
+  (widget-delete main-career))
+
+(defun main-screen ()
+  "Primary game screen - allows selecting between other screens"
+  (force-erase-buffer)
+  (widget-insert (format "You are %s, supreme ruler of %s.
+From here you can see all your dominion:\n\n" player-name univ-name))
+  (setq main-finance (widget-create 'push-button
+                                         :notify (lambda (&rest ignore)
+                                                   (cleanup-main)
+                                                   (finance-screen))
+                                         "Bursar's Office"))
+  (spacer) 
+  (setq main-admissions (widget-create 'push-button
                                             :notify (lambda (&rest ignore)
                                                       (cleanup-main)
-                                                      (academics-screen))
-                                            "Dean's Office"))
-   (spacer)
-   (setq simu-main-housing (widget-create 'push-button
-                                          :notify (lambda (&rest ignore)
-                                                    (cleanup-main)
-                                                    (housing-screen))
-                                          "Housing Office"))
-   (spacer)
-   (setq simu-main-dining (widget-create 'push-button
+                                                      (admissions-screen))
+                                            "Admissions Office"))
+  (spacer)
+  (setq main-academics (widget-create 'push-button
+                                           :notify (lambda (&rest ignore)
+                                                     (cleanup-main)
+                                                     (academics-screen))
+                                           "Dean's Office"))
+  (spacer)
+  (setq main-housing (widget-create 'push-button
                                          :notify (lambda (&rest ignore)
-                                                        (cleanup-main)
-                                                        (dining-screen))
-                                         "Dining Office"))
-   (spacer)
-   (setq simu-main-sports (widget-create 'push-button 
-                                         :notify (lambda (&rest ignore)
-                                                        (cleanup-main)  
-                                                        (sports-screen))
-                                         "Coach"))
-   (spacer)
-   (setq simu-main-career (widget-create 'push-button
-                                         :notify (lambda (&rest ignore)
-                                                        (cleanup-main)
-                                                        (career-screen))
-                                         "Career Center"))
-   (widget-setup))
+                                                   (cleanup-main)
+                                                   (housing-screen))
+                                         "Housing Office"))
+  (spacer)
+  (setq main-dining (widget-create 'push-button
+                                        :notify (lambda (&rest ignore)
+                                                  (cleanup-main)
+                                                  (dining-screen))
+                                        "Dining Office"))
+  (spacer)
+  (setq main-sports (widget-create 'push-button 
+                                        :notify (lambda (&rest ignore)
+                                                  (cleanup-main)  
+                                                  (sports-screen))
+                                        "Coach"))
+  (spacer)
+  (setq main-career (widget-create 'push-button
+                                        :notify (lambda (&rest ignore)
+                                                  (cleanup-main)
+                                                  (career-screen))
+                                        "Career Center"))
+  (widget-setup))
+
+(defvar interest 5)
+(defconst tuition-amt 3)
+(defconst finaid-amt 1)
+(defvar endowment 100)
+(defvar enrollment 200)
+
+(defun finance-interest-income ()
+  (/ (* endowment interest) 100))
+
+(defun finance-tuition-income ()
+  (* enrollment (- tuition-amt finaid-amt)))
+
+(defun finance-income ()
+  (+ (finance-interest-income) (finance-tuition-income)))
+
+
+(defvar finance-tuition)
+(defvar finance-finaid)
+
+(defun finish-finance ()
+  ;; TODO - handle errors
+  (setq tuition-amt (string-to-number (widget-value finance-tuition)))
+  (setq finaid-amt (string-to-number (widget-value finance-finaid)))
+  (widget-delete finance-tuition)
+  (widget-delete finance-finaid))
+
+(defun finance-screen ()
+  "The bursar's office where you can manage your endowment"
+  (force-erase-buffer)
+  (widget-insert (format "Endowment: %dK\n" endowment))
+  (widget-insert (format "Interest rate: %d%%\n" interest))
+  (widget-insert (format "Income from interest: %dK\n\n" (finance-interest-income)))
+  (widget-insert (format "Enrollment: %d students\n" enrollment))
+  (setq finance-tuition (widget-create 'editable-field
+                                :size 3
+                                :format "Tuition: $%vK"
+                                (number-to-string tuition-amt)
+                                ))
+  (widget-insert "\n")
+  (setq finance-finaid (widget-create 'editable-field
+                              :size 3  
+                              :format "Average financial aid: $%vK"
+                              (number-to-string finaid-amt)))
+  (widget-insert "\n")
+  (widget-insert (format "Net income from tuition: $%dK\n" (finance-tuition-income)))
+  (widget-insert (format "Total net income: $%dK\n\n" (finance-income)))
+
+  (widget-create 'push-button
+                 :notify (lambda (&rest ignore)
+                           (finish-finance)
+                           (finance-screen))
+                 "Recalculate")
+  (widget-insert " ")
+  (widget-create 'push-button
+                 :notify (lambda (&rest ignore)
+                           (finish-finance)
+                           (main-screen))
+                 "Done!")
+  (widget-setup))
