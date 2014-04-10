3d7e5c505fd3e485f170599d5b8771619552218e
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2014-04-09 20:47:49 -0400

    added markov chain generation, bible as test set

--- SimU/simu.el
+++ SimU/simu.el
@@ -37,7 +37,7 @@
 (defun print-stats ()
   (widget-insert (propertize (format "  Year: %d      Endowment: $%dK     Enrollment: %d     Prestige: %d\n"
                                      year endowment enrollment prestige)
-                             'font-lock-face 
+                             'font-lock-face
                              '(:foreground "white" :background "black")))
   (widget-insert "\n"))
 
@@ -52,18 +52,18 @@
 (defun start-screen ()
   "Draw the start screen and respond to user input"
   (force-erase-buffer)
-        (remove-overlays)
+  (remove-overlays)
   (use-local-map widget-keymap)
-  (widget-insert 
+  (widget-insert
 "***********       Welcome to SimU       ***********\n
 The year is 1900. You, fine sir or madam Emacs user,
 have been elected President / Prime Minister / Supreme
-Benefactor of a cozy little no-name college. Your 
+Benefactor of a cozy little no-name college. Your
 mission, should you chose to accept it, is to take this
 college and transform it over the next century or so
 into the institution of your chosing, whether it be
 and elite little private school, an elite not-so-little
-private school, or burgeoning state school. 
+private school, or burgeoning state school.
 
 ")
   (setq start-player-name (widget-create 'editable-field
@@ -101,17 +101,26 @@ private school, or burgeoning state school.
   (widget-delete main-advance))
 
 (defun advance-year ()
+  (setq endowment (- endowment (academic-expenses)))
   (setq endowment (+ endowment (finance-profit)))
   (setq year (1+ year))
   ;; 1 class graduates, we get some more from admissions
   (setq enrollment (round (+ (* .75 enrollment) (admissions-enrolling))))
-  (setq student-iq (admissions-iq)))
+  (setq student-iq (admissions-iq))
+  (if (> endowment 0)
+      (main-screen)
+    (fail-screen)))
+
+
+(defun fail-screen ()
+    (force-erase-buffer)
+    (widget-insert (format "You died of dysentery and/or bankruptcy.\nYou lasted until year %d.\n" year)))
 
 (defun main-screen ()
   "Primary game screen - allows selecting between other screens"
   (force-erase-buffer)
   (print-stats)
-  (widget-insert 
+  (widget-insert
 "From here you can visit different offices to control
 different aspects of the university:
 
@@ -121,7 +130,7 @@ different aspects of the university:
                                                    (cleanup-main)
                                                    (finance-screen))
                                          "Bursar's Office"))
-  (spacer) 
+  (spacer)
   (setq main-admissions (widget-create 'push-button
                                             :notify (lambda (&rest ignore)
                                                       (cleanup-main)
@@ -146,9 +155,9 @@ different aspects of the university:
                                                   (dining-screen))
                                         "Dining Office"))
   (spacer)
-  (setq main-sports (widget-create 'push-button 
+  (setq main-sports (widget-create 'push-button
                                         :notify (lambda (&rest ignore)
-                                                  (cleanup-main)  
+                                                  (cleanup-main)
                                                   (sports-screen))
                                         "Coach"))
   (spacer)
@@ -166,8 +175,7 @@ different aspects of the university:
   (setq main-advance (widget-create 'push-button
                                     :notify (lambda (&rest ignore)
                                               (cleanup-main)
-                                              (advance-year)
-                                              (main-screen))
+                                              (advance-year))
                                     "Advance to Next Year"))
   (setup-screen))
 
@@ -181,9 +189,12 @@ different aspects of the university:
   "Income from all sources, after accounting for financial aid"
   (+ (finance-interest-income) (finance-tuition-income)))
 
+(defun finance-expenses ()
+  (academic-expenses))
+
 (defun finance-profit ()
   "Income less expenses"
-  (finance-income))
+  (- (finance-income) (finance-expenses)))
 
 
 (defvar finance-tuition)
@@ -211,36 +222,14 @@ different aspects of the university:
                                 ))
   (widget-insert "\n")
   (setq finance-finaid (widget-create 'editable-field
-                              :size 3  
-                              :format "Average financial aid: $%vK"
-                              (number-to-string finaid-amt)))
-  (widget-insert "\n")
-  (widget-insert (format "Net income from tuition: $%dK\n" (finance-tuition-income)))
-  (widget-insert (format "Total net income: $%dK\n\n" (finance-income))))
-
-
-(defun finance-screen ()
-  "The bursar's office where you can manage your endowment"
-  (force-erase-buffer)
-  (print-stats)
-  (widget-insert (format "Endowment: %dK\n" endowment))
-  (widget-insert (format "Interest rate: %d%%\n" interest))
-  (widget-insert (format "Income from interest: %dK\n\n" (finance-interest-income)))
-  (widget-insert (format "Enrollment: %d students\n" enrollment))
-  (setq finance-tuition (widget-create 'editable-field
-                                :size 3
-                                :format "Tuition: $%vK"
-                                (number-to-string tuition-amt)
-                                ))
-  (widget-insert "\n")
-  (setq finance-finaid (widget-create 'editable-field
-                              :size 3  
+                              :size 3
                               :format "Average financial aid: $%vK"
                               (number-to-string finaid-amt)))
   (widget-insert "\n")
   (widget-insert (format "Net income from tuition: $%dK\n" (finance-tuition-income)))
   (widget-insert (format "Total net income: $%dK\n\n" (finance-income)))
-
+  (widget-insert (format "Total expenses: $%dK\n" (finance-expenses)))
+  (widget-insert (format "Total profit: $%dK\n\n" (finance-profit)))
   (widget-create 'push-button
                  :notify (lambda (&rest ignore)
                            (finish-finance)
@@ -283,7 +272,7 @@ different aspects of the university:
   ;; everyone, you get ~70 which is pretty dumb. Note you can also accept more than everyone
   ;; which should have no additional effect
   (let ((max-iq 170))
-    (- max-iq (/ (* 100 (min students-accepted (admissions-applicants))) 
+    (- max-iq (/ (* 100 (min students-accepted (admissions-applicants)))
                  (admissions-applicants)))))
 
 
@@ -308,7 +297,7 @@ different aspects of the university:
   (force-erase-buffer)
   (print-stats)
   (setq admissions-hire (widget-create 'push-button
-                 :format (concat "%[[Hire Admissions Counselor]%]" 
+                 :format (concat "%[[Hire Admissions Counselor]%]"
                                  (format " ($%dK)" counselor-cost))
                  :notify (lambda (&rest ignore)
                            (buy-counselor)
@@ -316,9 +305,9 @@ different aspects of the university:
                            (admissions-screen))))
   (spacer)
   (setq admissions-scholarship (widget-create 'push-button
-                 :format (concat "%[[Create Merit-Based Scholarship]%]" 
+                 :format (concat "%[[Create Merit-Based Scholarship]%]"
                                  (format " ($%dK)" scholarship-cost))
-                 :notify (lambda (&rest ignore) 
+                 :notify (lambda (&rest ignore)
                            (buy-scholarship)
                            (finish-admissions)
                            (admissions-screen))))
@@ -326,7 +315,7 @@ different aspects of the university:
   (widget-insert (format "Estimated applicants: %d\n\n" (admissions-applicants)))
   (setq admissions-accept (widget-create 'editable-field
                  :size 10
-                 :format "Max # Accepted: %v \n\n"  
+                 :format "Max # Accepted: %v \n\n"
                  (number-to-string students-accepted)))
   (widget-insert (format "Estimated enrolling: %d\n\n" (admissions-enrolling)))
   (widget-insert (format "Estimated avg. student I.Q.: %d \n\n" (admissions-iq)))
@@ -338,6 +327,71 @@ different aspects of the university:
   (widget-insert " ")  (widget-create 'push-button
                  :notify (lambda (&rest ignore)
                            (finish-admissions)
+                           (main-screen))
+                 "Done!")
+  (setup-screen))
+
+
+(defun finish-academics ()
+  (setq teaching-profs (string-to-number (widget-value academics-teaching-profs)))
+  (setq research-profs (string-to-number (widget-value academics-research-profs)))
+  (setq prof-salary (string-to-number (widget-value academics-prof-salary)))
+  (setq ta-budget (string-to-number (widget-value academics-ta-budget)))
+  (setq ra-budget (string-to-number (widget-value academics-ra-budget)))
+  (widget-delete academics-teaching-profs)
+  (widget-delete academics-research-profs)
+  (widget-delete academics-prof-salary)
+  (widget-delete academics-ta-budget)
+  (widget-delete academics-ra-budget))
+
+(defvar teaching-profs)
+(defvar research-profs)
+(defvar prof-salary)
+(defvar ta-budget)
+(defvar ra-budget)
+(setq teaching-profs 10)
+(setq research-profs 10)
+(setq prof-salary 80)
+(setq ta-budget 40)
+(setq ra-budget 10)
+
+(defun academic-expenses ()
+  (+ ta-budget ra-budget (* prof-salary (+ teaching-profs research-profs))))
+
+(defun academics-screen ()
+  (force-erase-buffer)
+  (print-stats)
+  (setq academics-teaching-profs (widget-create 'editable-field
+                 :size 10
+                 :format "Teaching Professors: %v \n\n"
+                 (number-to-string teaching-profs)))
+  (setq academics-research-profs (widget-create 'editable-field
+                 :size 10
+                 :format "Research Professors: %v \n\n"
+                 (number-to-string research-profs)))
+  (setq academics-prof-salary (widget-create 'editable-field
+                 :size 10
+                 :format "Avg. Professor Salary: %v \n\n"
+                 (number-to-string prof-salary)))
+  (setq academics-ta-budget (widget-create 'editable-field
+                 :size 10
+                 :format "TA Budget: %v \n\n"
+                 (number-to-string ta-budget)))
+  (setq academics-ra-budget (widget-create 'editable-field
+                 :size 10
+                 :format "RA Budget: %v \n\n"
+                 (number-to-string ra-budget)))
+  (widget-insert (format "Students per professor: %d\n\n" (/ enrollment teaching-profs)))
+  (widget-insert (format "Academic expenses: %dK\n\n" (enrollment-expenses)))
+  (widget-create 'push-button
+                 :notify (lambda (&rest ignore)
+                           (finish-academics)
+                           (academics-screen))
+                 "Recalculate")
+  (widget-insert " ")
+  (widget-create 'push-button
+                 :notify (lambda (&rest ignore)
+                           (finish-academics)
                            (main-screen))
                  "Done!")
   (setup-screen))
--- correct/correct.el
+++ correct/correct.el
@@ -1,4 +1,4 @@
-
+(load "~/private/emacs/correct/bible")
 (require 'cl)
 (defun memo-table (n m)
   (let ((memo (make-vector n nil)))
@@ -27,12 +27,70 @@
 (levenshtein "scattergory" "category")
 
 
-(defun pairs (L) "All pairs of consecutive elements in L"
+(defun pairs-t (L acc) "All pairs of consecutive elements in L"
   (if (null L)
-      nil
+      acc
     (if (null (cdr L))
-        nil
-      (cons (cons (car L) (cadr L)) (pairs (cdr L))))))
+        acc
+      (pairs-t (cdr L) (cons (cons (car L) (cadr L)) acc)))))
+
+(defun pairs (L)
+  (let ((result nil))
+    (while (not (null L))
+      (setq result (cons (cons (car L) (cadr L)) result))
+      (setq L (cdr L)))
+    result))
+
+(defun add-count (hash prev next)
+  (if (null (gethash prev hash))
+      (puthash prev (cons 0 (make-hash-table :test 'equal)) hash))
+  (let* ((count-prevhash (gethash prev hash))
+         (count (car count-prevhash))
+         (prevhash (cdr count-prevhash))
+         (val (gethash next prevhash)))
+    (puthash next (+ 1 (if (null val) 0 val)) prevhash)
+    (puthash prev (cons (+ 1 count) prevhash) hash))
+  hash)
+
+(defun do-count (pairs counts)
+  (while (not (null pairs))
+    (setq counts (add-count counts (caar pairs) (cdar pairs)))
+    (setq pairs (cdr pairs)))
+  counts)
+
+(defun freq-of-count (hash)
+  (maphash
+   (lambda (prev count-prevhash)
+     (maphash (lambda (next next-count)
+                (puthash next (cons next-count (/ (float next-count) (float (car count-prevhash)))) (cdr count-prevhash)))
+              (cdr count-prevhash))
+     (puthash prev (cdr count-prevhash) hash))
+   hash)
+  (let ((freq-list nil))
+    (maphash
+     (lambda (prev nexthash)
+       (maphash
+        (lambda (next freq)
+          (setq freq-list (cons (cons prev (cons next freq)) freq-list)))
+        nexthash))
+     hash)
+    freq-list))
+
+(defun sort-by-freq (rules)
+  (sort rules (lambda (rule1 rule2) (> (car (cddr rule1)) (car (cddr rule2))))))
+
+(defun take-t (L n acc)
+  (if (< n 1) acc
+    (if (null L) acc
+      (take-t (cdr L) (- n 1) (cons (car L) acc)))))
+
+(defun take (L n)
+  (let ((acc nil))
+    (while (not (or (null L) (<= n 0)))
+      (setq acc (cons (car L) acc))
+      (setq L (cdr L)))
+    (reverse acc)))
+
 
 (defun markov-rules (corpus rule-count)
   "Given a CORPUS string, generates a set of RULE-COUNT prediction rules based on the
@@ -43,3 +101,7 @@
          (counts (do-count pair-list (make-hash-table :test 'equal)))
          (freqs (freq-of-count counts)))
     (take (sort-by-freq freqs) rule-count)))
+
+(setq kgrams (markov-rules "sup dawg yo mang" 1000))
+
+(nth 800 kgrams)
